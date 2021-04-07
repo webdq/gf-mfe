882e0e3432617be21fdf5abccb96365ab39bf631
dengqiang <13716834942@qq.com>
2021-04-07 14:27:14 +0800

    添加vue应用

---
+++ app-vue/prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true, // 语句末尾分号
+  singleQuote: true, // 单引号
+  jsxSingleQuote: false, // 在JSX中使用单引号
+  trailingComma: 'none', // 多行时尽可能打印尾随逗号
+  bracketSpacing: true, // 对象的括号前后空格
+  jsxBracketSameLine: false, // 将多行JSX元素的>放在最后一行的末尾
+  arrowParens: 'always', // 在单独的箭头函数参数周围加上括号
+  endOfLine: 'auto', // "<lf|crlf|cr|auto>"
+  spaceBeforeParen: true
+}
